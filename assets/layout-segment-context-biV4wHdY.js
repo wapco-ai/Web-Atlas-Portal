d3905eb70166ca2a0b8b4daf7ecbb9426918eecd
Wapco-tech <wappo.dev@gmail.com>
2026-08-03 16:23:33 +0330

    Updates

--- assets/layout-segment-context-biV4wHdY.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./rolldown-runtime-S-ySWqyJ.js";import{i as t}from"./framework-B8WyT5R3.js";import{r as n}from"./index-jGNNw6-s.js";var r=e(t(),1);function i({segmentMap:e,children:t}){let i=n();return i?(0,r.createElement)(i.Provider,{value:e},t):t}export{i as LayoutSegmentProvider};
